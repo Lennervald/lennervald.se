a80a0002bdc8289e0c2260ee7bfea8d138591ca5
Martin Lennervald <martin.lennervald@gmail.com>
2017-12-21 02:33:49 +0100

    test of auto-git

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('*',(req,res) => res.send('lennervald.se'));
+app.get('*',(req,res) => res.send('lennervald.se - just a test'));
 
 app.listen(3000,()=>console.log('lennervald.se listening on port 3000'));
